0800158828a022d9e6b821adb17b8bda533f207e
Midas0615 <b4251154@gmail.com>
2020-05-11 11:11:54 +0300

    Update the nuxt routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 export default {
   mode: 'universal',
   /*
@@ -95,6 +97,60 @@ export default {
 
       ]
   },
+  generate: {
+    exclude: [
+      /^(?=.*\bignore\b).*$/
+    ],
+    routes: function () {
+      return axios.get("https://api.storyblok.com/v1/cdn/stories?version=published&token=ZkIEqD1i8FVF628krYkPhAtt&cv=" +
+      Math.floor(Date.now() / 1e3)
+      )
+      .then ( res=> {
+        const pages = res.data.stories.map(pg => pg.full_slug)
+        // console.log("------------------------fdsfdsfsdfsdf-------------------", res.data.stories)
+          return [
+            '/',
+            '/vic',
+            '/qld',
+            '/nsw',
+            '/global',
+            '/category',
+            ...pages
+          ]
+        }
+      )
+    },
+    },   
+    // routes: function (callback) {
+    //   const token = `ZkIEqD1i8FVF628krYkPhAtt`
+    //   const version = 'published'
+    //   let cache_version = 0
+
+    //   let toIgnore = ['home', 'en/settings']
+
+    //    // other routes that are not in Storyblok with their slug.
+    //   let routes = ['/'] // adds / directly
+
+    //    // Load space and receive latest cache version key to improve performance
+    //   axios.get(`https://api.storyblok.com/v1/cdn/spaces/me?token=${token}`).then((space_res) => {
+
+    //      // timestamp of latest publish
+    //     cache_version = space_res.data.space.version
+    //     console.log("content---------------", space_res)
+
+    //      // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
+    //     axios.get(`https://api.storyblok.com/v1/cdn/links?token=${token}&version=${version}&cv=${cache_version}&per_page=100`).then((res) => {
+    //       Object.keys(res.data.links).forEach((key) => {
+    //         if (!toIgnore.includes(res.data.links[key].slug)) {
+    //           routes.push('/' + res.data.links[key].slug)
+    //         }
+    //       })
+
+    //       callback(null, routes)
+    //     })
+    //   }) 
+    // }
+  // },
   /*
   ** Build configuration
   */
