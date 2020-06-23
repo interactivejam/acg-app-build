b82f3496bda48d5b96daadb81ea0f7ab7c839804
Midas0615 <b4251154@gmail.com>
2020-06-23 11:18:29 +0300

    Fix the rich-text issue on the faq-page

--- components/Homepage.vue
+++ components/Homepage.vue
@@ -43,6 +43,7 @@
 
         <div class="container">
             <div class="row">
+                <p>{{blok}}</p>
                 <div class="col-xl-6 col-md-6 col-sm-12" v-editable="blok">
                     <component v-if="blok.component === 'rich-text'" :key="blok._uid" v-for="blok in blok.body" :blok="blok" :is="blok.component"></component>
                 </div>
